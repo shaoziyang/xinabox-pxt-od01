f71ae83cbc3a230b17cda1863f23a9d63b00689b
shaoziyang <shaoziyang@126.com>
2019-07-09 00:36:03 +0800

    Update od01.ts
    change font from 5x5 to 5x7
    add print function

--- od01.ts
+++ od01.ts
@@ -4,6 +4,9 @@
  *   https://github.com/makecode-extensions/OLED12864_I2C
  */
 
+// 6x8 font
+const Font_5x7 = hex`000000000000005F00000007000700147F147F14242A072A12231308646237495522500005030000001C2241000041221C00082A1C2A0808083E080800503000000808080808006060000020100804023E5149453E00427F400042615149462141454B311814127F1027454545393C4A49493001710905033649494936064949291E003636000000563600000008142241141414141441221408000201510906324979413E7E1111117E7F494949363E414141227F4141221C7F494949417F090901013E414151327F0808087F00417F41002040413F017F081422417F404040407F0204027F7F0408107F3E4141413E7F090909063E4151215E7F09192946464949493101017F01013F4040403F1F2040201F7F2018207F63140814630304780403615149454300007F4141020408102041417F000004020102044040404040000102040020545454787F484444383844444420384444487F3854545418087E090102081454543C7F0804047800447D40002040443D00007F10284400417F40007C041804787C0804047838444444387C14141408081414187C7C080404084854545420043F4440203C4040207C1C2040201C3C4030403C44281028440C5050503C4464544C44000836410000007F000000413608000201020402`
+
 //% weight=50 color=#081620 icon="O" block="OD01"
 namespace OD01 {
     export enum DISPLAY_ONOFF {
@@ -13,143 +16,16 @@ namespace OD01 {
         DISPLAY_OFF = 0
     }
 
-    let font: number[] = [];
-    font[0] = 0x0022d422;
-    font[1] = 0x0022d422;
-    font[2] = 0x0022d422;
-    font[3] = 0x0022d422;
-    font[4] = 0x0022d422;
-    font[5] = 0x0022d422;
-    font[6] = 0x0022d422;
-    font[7] = 0x0022d422;
-    font[8] = 0x0022d422;
-    font[9] = 0x0022d422;
-    font[10] = 0x0022d422;
-    font[11] = 0x0022d422;
-    font[12] = 0x0022d422;
-    font[13] = 0x0022d422;
-    font[14] = 0x0022d422;
-    font[15] = 0x0022d422;
-    font[16] = 0x0022d422;
-    font[17] = 0x0022d422;
-    font[18] = 0x0022d422;
-    font[19] = 0x0022d422;
-    font[20] = 0x0022d422;
-    font[21] = 0x0022d422;
-    font[22] = 0x0022d422;
-    font[23] = 0x0022d422;
-    font[24] = 0x0022d422;
-    font[25] = 0x0022d422;
-    font[26] = 0x0022d422;
-    font[27] = 0x0022d422;
-    font[28] = 0x0022d422;
-    font[29] = 0x0022d422;
-    font[30] = 0x0022d422;
-    font[31] = 0x0022d422;
-    font[32] = 0x00000000;
-    font[33] = 0x000002e0;
-    font[34] = 0x00018060;
-    font[35] = 0x00afabea;
-    font[36] = 0x00aed6ea;
-    font[37] = 0x01991133;
-    font[38] = 0x010556aa;
-    font[39] = 0x00000060;
-    font[40] = 0x000045c0;
-    font[41] = 0x00003a20;
-    font[42] = 0x00051140;
-    font[43] = 0x00023880;
-    font[44] = 0x00002200;
-    font[45] = 0x00021080;
-    font[46] = 0x00000100;
-    font[47] = 0x00111110;
-    font[48] = 0x0007462e;
-    font[49] = 0x00087e40;
-    font[50] = 0x000956b9;
-    font[51] = 0x0005d629;
-    font[52] = 0x008fa54c;
-    font[53] = 0x009ad6b7;
-    font[54] = 0x008ada88;
-    font[55] = 0x00119531;
-    font[56] = 0x00aad6aa;
-    font[57] = 0x0022b6a2;
-    font[58] = 0x00000140;
-    font[59] = 0x00002a00;
-    font[60] = 0x0008a880;
-    font[61] = 0x00052940;
-    font[62] = 0x00022a20;
-    font[63] = 0x0022d422;
-    font[64] = 0x00e4d62e;
-    font[65] = 0x000f14be;
-    font[66] = 0x000556bf;
-    font[67] = 0x0008c62e;
-    font[68] = 0x0007463f;
-    font[69] = 0x0008d6bf;
-    font[70] = 0x000094bf;
-    font[71] = 0x00cac62e;
-    font[72] = 0x000f909f;
-    font[73] = 0x000047f1;
-    font[74] = 0x0017c629;
-    font[75] = 0x0008a89f;
-    font[76] = 0x0008421f;
-    font[77] = 0x01f1105f;
-    font[78] = 0x01f4105f;
-    font[79] = 0x0007462e;
-    font[80] = 0x000114bf;
-    font[81] = 0x000b6526;
-    font[82] = 0x010514bf;
-    font[83] = 0x0004d6b2;
-    font[84] = 0x0010fc21;
-    font[85] = 0x0007c20f;
-    font[86] = 0x00744107;
-    font[87] = 0x01f4111f;
-    font[88] = 0x000d909b;
-    font[89] = 0x00117041;
-    font[90] = 0x0008ceb9;
-    font[91] = 0x0008c7e0;
-    font[92] = 0x01041041;
-    font[93] = 0x000fc620;
-    font[94] = 0x00010440;
-    font[95] = 0x01084210;
-    font[96] = 0x00000820;
-    font[97] = 0x010f4a4c;
-    font[98] = 0x0004529f;
-    font[99] = 0x00094a4c;
-    font[100] = 0x000fd288;
-    font[101] = 0x000956ae;
-    font[102] = 0x000097c4;
-    font[103] = 0x0007d6a2;
-    font[104] = 0x000c109f;
-    font[105] = 0x000003a0;
-    font[106] = 0x0006c200;
-    font[107] = 0x0008289f;
-    font[108] = 0x000841e0;
-    font[109] = 0x01e1105e;
-    font[110] = 0x000e085e;
-    font[111] = 0x00064a4c;
-    font[112] = 0x0002295e;
-    font[113] = 0x000f2944;
-    font[114] = 0x0001085c;
-    font[115] = 0x00012a90;
-    font[116] = 0x010a51e0;
-    font[117] = 0x010f420e;
-    font[118] = 0x00644106;
-    font[119] = 0x01e8221e;
-    font[120] = 0x00093192;
-    font[121] = 0x00222292;
-    font[122] = 0x00095b52;
-    font[123] = 0x0008fc80;
-    font[124] = 0x000003e0;
-    font[125] = 0x000013f1;
-    font[126] = 0x00841080;
-    font[127] = 0x0022d422;
-
-    let _I2CAddr = 60;
-    let _screen = pins.createBuffer(1025);
-    let _buf2 = pins.createBuffer(2);
-    let _buf3 = pins.createBuffer(3);
-    let _buf4 = pins.createBuffer(4);
-    let _ZOOM = 1;
-    let _DRAW = 1;
+    let _I2CAddr = 60
+    let _screen = pins.createBuffer(1025)
+    let _buf2 = pins.createBuffer(2)
+    let _buf3 = pins.createBuffer(3)
+    let _buf4 = pins.createBuffer(4)
+    let _buf7 = pins.createBuffer(7)
+    _buf7[0] = 0x40
+    let _DRAW = 1
+    let _cx = 0
+    let _cy = 0
 
     function cmd1(d: number) {
         let n = d % 256;
@@ -173,9 +49,8 @@ namespace OD01 {
 
     function set_pos(col: number = 0, page: number = 0) {
         cmd1(0xb0 | page) // page number
-        let c = col * (_ZOOM + 1)
-        cmd1(0x00 | (c % 16)) // lower start column address
-        cmd1(0x10 | (c >> 4)) // upper start column address    
+        cmd1(0x00 | (col % 16)) // lower start column address
+        cmd1(0x10 | (col >> 4)) // upper start column address    
     }
 
     // clear bit
@@ -206,74 +81,90 @@ namespace OD01 {
     export function pixel(x: number, y: number, color: number = 1) {
         let page = y >> 3
         let shift_page = y % 8
-        let ind = x * (_ZOOM + 1) + page * 128 + 1
+        let ind = x + page * 128 + 1
         let b = (color) ? (_screen[ind] | (1 << shift_page)) : clrbit(_screen[ind], shift_page)
         _screen[ind] = b
-        set_pos(x, page)
-        if (_ZOOM) {
-            _screen[ind + 1] = b
-            if (_DRAW) {
-                _buf3[0] = 0x40
-                _buf3[1] = _buf3[2] = b
-                pins.i2cWriteBuffer(_I2CAddr, _buf3)
-            }
+        if (_DRAW) {
+            set_pos(x, page)
+            _buf2[0] = 0x40
+            _buf2[1] = b
+            pins.i2cWriteBuffer(_I2CAddr, _buf2)
         }
-        else {
-            if (_DRAW) {
-                _buf2[0] = 0x40
-                _buf2[1] = b
-                pins.i2cWriteBuffer(_I2CAddr, _buf2)
-            }
+    }
+
+    function char(c: string, col: number, row: number, color: number = 1) {
+        let p = (Math.min(127, Math.max(c.charCodeAt(0), 32)) - 32) * 5
+        let ind = col + row * 128 + 1
+
+        for (let i = 0; i < 5; i++) {
+            _screen[ind + i] = (color > 0) ? Font_5x7[p + i] : Font_5x7[p + i] ^ 0xFF
+            _buf7[i + 1] = _screen[ind + i]
         }
+        _screen[ind + 5] = (color > 0) ? 0 : 0xFF
+        _buf7[6] = _screen[ind + 5]
+        set_pos(col, row)
+        pins.i2cWriteBuffer(_I2CAddr, _buf7)
     }
 
     /**
      * show text in OLED
      */
-    //% blockId="OLED12864_I2C_SHOWSTRING" block="show string text %s|at x %x|y %y|color %color"
+    //% blockId="OLED12864_I2C_SHOWSTRING" block="show string %s|at col %col|row %row|color %color"
     //% s.defl='Hello'
-    //% x.max=127 x.min=0 x.defl=0
-    //% y.max=7 y.min=0 y.defl=0
+    //% col.max=120 col.min=0 col.defl=0
+    //% row.max=7 row.min=0 row.defl=0
     //% color.max=1 color.min=0 color.defl=1
     //% weight=80 blockGap=8 inlineInputMode=inline
-    export function String(s: string, x: number, y: number, color: number = 1) {
-        let col = 0
-        let p = 0
-        let ind = 0
+    export function String(s: string, col: number, row: number, color: number = 1) {
         for (let n = 0; n < s.length; n++) {
-            p = font[s.charCodeAt(n)]
-            for (let i = 0; i < 5; i++) {
-                col = 0
-                for (let j = 0; j < 5; j++) {
-                    if (p & (1 << (5 * i + j)))
-                        col |= (1 << (j + 1))
-                }
-                ind = (x + n) * 5 * (_ZOOM + 1) + y * 128 + i * (_ZOOM + 1) + 1
-                if (color == 0)
-                    col = 255 - col
-                _screen[ind] = col
-                if (_ZOOM)
-                    _screen[ind + 1] = col
-            }
+            char(s.charAt(n), col, row, color)
+            col += 6
         }
-        set_pos(x * 5, y)
-        let ind0 = x * 5 * (_ZOOM + 1) + y * 128
-        let buf = _screen.slice(ind0, ind + 1)
-        buf[0] = 0x40
-        pins.i2cWriteBuffer(_I2CAddr, buf)
     }
 
     /**
      * show a number in OLED
      */
-    //% blockId="OLED12864_I2C_NUMBER" block="show Number %num|at x %x|y %y|color %color"
+    //% blockId="OLED12864_I2C_NUMBER" block="show Number %num|at col %col|row %row|color %color"
     //% num.defl=100
-    //% x.max=127 x.min=0 x.defl=0
-    //% y.max=7 y.min=0 y.defl=0
+    //% col.max=120 col.min=0 col.defl=0
+    //% row.max=7 row.min=0 row.defl=0
     //% color.max=1 color.min=0 color.defl=1
     //% weight=80 blockGap=8 inlineInputMode=inline
-    export function Number(num: number, x: number, y: number, color: number = 1) {
-        String(num.toString(), x, y, color)
+    export function Number(num: number, col: number, row: number, color: number = 1) {
+        String(num.toString(), col, row, color)
+    }
+
+    function scroll() {
+        _cx = 0
+        _cy++
+        if (_cy > 7) {
+            _cy = 7
+            _screen.shift(128)
+            _screen[0] = 0x40
+            draw(1)
+        }
+    }
+
+    /**
+     * print a text in OLED
+     */
+    //% block="print %s|color %color|newline %newlin"
+    //% s.defl="string"
+    //% color.max=1 color.min=0 color.defl=1
+    //% newline.defl=true
+    //% weight=80 blockGap=8 inlineInputMode=inline
+    export function print(s: string, color: number, newline: boolean = true) {
+        for (let n = 0; n < s.length; n++) {
+            char(s.charAt(n), _cx, _cy, color)
+            _cx += 6
+            if (_cx > 120) {
+                scroll()
+            }
+        }
+        if (newline) {
+            scroll()
+        }
     }
 
     /**
@@ -366,16 +257,6 @@ namespace OD01 {
     }
 
     /**
-     * zoom mode
-     */
-    //% blockId="OLED12864_I2C_ZOOM" block="zoom %d"
-    //% weight=60 blockGap=8
-    export function zoom(d: boolean = true) {
-        _ZOOM = (d) ? 1 : 0
-        cmd2(0xd6, _ZOOM)
-    }
-
-    /**
      * OLED initialize
      */
     //% blockId="OLED12864_I2C_init" block="Initial OLED"
@@ -398,10 +279,9 @@ namespace OD01 {
         cmd2(0xd9, 0xF1) // SSD1306_SETPRECHARGE
         cmd2(0xDB, 0x40) // SSD1306_SETVCOMDETECT
         cmd1(0xA6)       // SSD1306_NORMALDISPLAY
-        cmd2(0xD6, 1)    // zoom on
+        cmd2(0xD6, 0)    // zoom off
         cmd1(0xAF)       // SSD1306_DISPLAYON
         clear()
-        _ZOOM = 1
     }
 
     init();
